dc853323411f94d663a6cd4aab0a81b9fb25becf
putriulhaqq <punyaulhaq@gmail.com>
2024-07-11 10:19:57 +0700

    add: add logging

--- api/db.py
+++ api/db.py
@@ -1,9 +1,13 @@
 import os
 import psycopg2.pool
 import atexit
+import logging
 from dotenv import load_dotenv
 
-# Load environment variables
+# Konfigurasi logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 load_dotenv()
 
 pool = None 
@@ -22,30 +26,37 @@ class ConnectionPool:
                 host=os.getenv('DB_HOST'),
                 port=os.getenv('DB_PORT')
             )
+            logger.debug("Connection pool created successfully")
         except psycopg2.OperationalError as e:
-            print(f"Failed to connect to database: {e}")
+            logger.error(f"Failed to connect to database: {e}")
             self.pool = None
 
     def get_connection(self):
         if self.pool:
+            logger.debug("Getting connection from pool")
             return self.pool.getconn()
         else:
+            logger.error("Connection pool is not initialized")
             raise Exception("Connection pool is not initialized")
 
     def return_connection(self, conn):
         if self.pool:
+            logger.debug("Returning connection to pool")
             self.pool.putconn(conn)
+        else:
+            logger.error("Cannot return connection. Pool is not initialized")
 
     def close_all_connections(self):
         if self.pool:
+            logger.debug("Closing all connections")
             self.pool.closeall()
+        else:
+            logger.error("Cannot close connections. Pool is not initialized")
 
 def initializeConnectionPool():
     global pool
     pool = ConnectionPool()
-    print(pool)
+    logger.debug(f"Initialized ConnectionPool: {pool}")
 
-# Uncomment the following line if you want to close connections on exit
+# Uncomment jika Anda ingin menutup koneksi saat keluar
 # atexit.register(lambda: pool.close_all_connections() if pool else None)
-
-# __all__ = ['pool', 'ConnectionPool', 'initializeConnectionPool']
